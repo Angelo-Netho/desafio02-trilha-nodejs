18b71e3a14fbcc63dd91401de014f339ba792001
Angelo-Netho <angelonetho@gmail.com>
2022-07-30 01:51:34 -0300

    Valida todo

--- src/index.js
+++ src/index.js
@@ -34,7 +34,25 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user) {
+    response.status(404).json({ error: "User not found"})
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if(!todo) {
+    response.status(404).json({ error: "Todo not found"})
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  next();
 }
 
 function findUserById(request, response, next) {
